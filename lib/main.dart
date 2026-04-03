cb452e13cbdeba771ba84d2a1a99b16b926175f8
Joshua Sanchez Ruiz <jjsanchez-ruiz@hotmail.com>
2026-04-03 14:19:38 +0900

    succesfully connect firebase with flutter application

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_helpvrywhere/firebase_options.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
