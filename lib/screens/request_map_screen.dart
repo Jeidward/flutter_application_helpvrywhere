a60c6284459032a85b0a319ba430e3373199f77a
Arbalight <Heliotness45@gmail.com>
2026-04-23 21:43:58 +0900

    Adding Location service for create and update the user's requests

--- lib/screens/request_map_screen.dart
+++ lib/screens/request_map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_helpvrywhere/services/location_service.dart';
+import 'package:geolocator/geolocator.dart';
 
 class RequestMapScreen extends StatelessWidget {
   const RequestMapScreen({super.key});
@@ -9,16 +10,29 @@ class RequestMapScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
       //current location
-      body: FutureBuilder<String>(
+      body: FutureBuilder<Position>(
         future: LocationService().getCurrentLocation(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            return Text(snapshot.data ?? 'No location found');
+            return const Center(child: CircularProgressIndicator());
           }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (!snapshot.hasData) {
+            return const Center(child: Text('No location found'));
+          }
+
+          final position = snapshot.data!;
+
+          return Center(
+            child: Text(
+              'Latitude: ${position.latitude}\nLongitude: ${position.longitude}',
+              textAlign: TextAlign.center,
+            ),
+          );
         },
       ),
     );
